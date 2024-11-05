25f4f7aa39c50460564a72bd91831074b1dd38f1
Irina Sitnikova <iri@MacBook-Pro-Irina.local>
2024-11-05 23:00:26 +0300

    Fix

---
+++ Hahahub/app/src/main/java/com/hahahub/data/Constants.kt
@@ -0,0 +1,7 @@
+package com.hahahub.data
+
+object Constants {
+    const val CATEGORY_KEY = "category"
+    const val QUESTION_KEY = "question"
+    const val ANSWER_KEY = "answer"
+}
--- Hahahub/app/src/main/java/com/hahahub/recycler/adapter/JokeAdapter.kt
+++ Hahahub/app/src/main/java/com/hahahub/recycler/adapter/JokeAdapter.kt
@@ -9,6 +9,7 @@ import com.hahahub.data.Joke
 import com.hahahub.recycler.JokeViewHolder
 import com.hahahub.recycler.util.JokeDiffUtilCallback
 import android.os.Bundle
+import com.hahahub.data.Constants
 
 class JokeAdapter: RecyclerView.Adapter<JokeViewHolder>() {
 
@@ -36,13 +37,7 @@ class JokeAdapter: RecyclerView.Adapter<JokeViewHolder>() {
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int, payloads: MutableList<Any>) {
         if (payloads.isNotEmpty()) {
             val diffBundle = payloads[0] as Bundle
-            diffBundle.keySet().forEach { key ->
-                when (key) {
-                    "category" -> holder.binding.jokeCategory.text = diffBundle.getString("category")
-                    "question" -> holder.binding.jokeQuestion.text = diffBundle.getString("question")
-                    "answer" -> holder.binding.jokeAnswer.text = diffBundle.getString("answer")
-                }
-            }
+            holder.bind(diffBundle)
         } else {
             holder.bind(data[position])
         }
