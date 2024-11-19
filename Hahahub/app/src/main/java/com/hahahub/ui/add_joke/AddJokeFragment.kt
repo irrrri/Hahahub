4a7f307caf7fdd37d6c6029f3210c810d4aa17f9
Irina Sitnikova <iri@MacBook-Pro-Irina.local>
2024-11-19 16:31:50 +0300

    Fix

--- Hahahub/app/src/main/java/com/hahahub/ui/add_joke/AddJokeFragment.kt
+++ Hahahub/app/src/main/java/com/hahahub/ui/add_joke/AddJokeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.hahahub.databinding.FragmentAddJokeBinding
 
 class AddJokeFragment : Fragment() {
@@ -33,14 +34,20 @@ class AddJokeFragment : Fragment() {
             val answer = binding.editAnswer.text.toString()
 
             if (category.isNotBlank() && question.isNotBlank() && answer.isNotBlank()) {
-                addJokeViewModel.addNewJoke(category, question, answer) {
-                    Toast.makeText(requireContext(), "Шутка добавлена!", Toast.LENGTH_SHORT).show()
-                    parentFragmentManager.popBackStack()
-                }
+                addJokeViewModel.addNewJoke(category, question, answer)
             } else {
                 Toast.makeText(requireContext(), "Заполните все поля!", Toast.LENGTH_SHORT).show()
             }
         }
+
+        addJokeViewModel.addJokeCompleted.observe(viewLifecycleOwner, Observer { isCompleted ->
+            if (isCompleted) {
+                Toast.makeText(requireContext(), "Шутка добавлена!", Toast.LENGTH_SHORT).show()
+                parentFragmentManager.popBackStack()
+            } else {
+                Toast.makeText(requireContext(), "Ошибка при добавлении шутки!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     override fun onDestroyView() {
--- Hahahub/app/src/main/java/com/hahahub/ui/add_joke/AddJokeViewModel.kt
+++ Hahahub/app/src/main/java/com/hahahub/ui/add_joke/AddJokeViewModel.kt
@@ -1,18 +1,25 @@
 package com.hahahub.ui.add_joke
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hahahub.data.Joke
 import com.hahahub.data.JokeRepository
 import kotlinx.coroutines.launch
 
 class AddJokeViewModel : ViewModel() {
 
-    fun addNewJoke(category: String, question: String, answer: String, onComplete: () -> Unit) {
-        viewModelScope.launch {
-            JokeRepository.addJoke(category, question, answer)
+    private val _addJokeCompleted = MutableLiveData<Boolean>()
+    val addJokeCompleted: LiveData<Boolean> get() = _addJokeCompleted
 
-            onComplete()
+    fun addNewJoke(category: String, question: String, answer: String) {
+        viewModelScope.launch {
+            try {
+                JokeRepository.addJoke(category, question, answer)
+                _addJokeCompleted.value = true
+            } catch (e: Exception) {
+                _addJokeCompleted.value = false
+            }
         }
     }
 }
